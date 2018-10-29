ea3985cc807f86842325c4f3ebe9207bca42ef38
kwichmann <kristian.wichmann@gmail.com>
2018-10-29 01:18:53 +0100

    Production added to knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -5,4 +5,8 @@ module.exports = {
     client: 'pg',
     connection: 'postgres://localhost/friendly-forum',
   },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+  },
 };
